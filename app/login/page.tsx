403f2f8f5c6d0ac026f71663ae9501b76a397168
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-10 15:58:14 +0200

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -78,7 +78,7 @@ export default function LoginPage() {
           </div>
           <div className="text-center">
             <Link href="/register" className="text-sm text-blue-600 hover:text-blue-500">
-              Don't have an account? Sign up
+              Don&apos;t have an account? Sign up
             </Link>
           </div>
         </form>
