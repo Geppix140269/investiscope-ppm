07aba61811e83ae80a4d73203e407b41bead6590
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-11 17:49:57 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import './global.css'
 import Navigation from './components/Navigation'
 
 const inter = Inter({ subsets: ['latin'] })
